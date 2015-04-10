f5e4bb7bb1641bc89e451514458eb3a20f4ca152
lancasja <lancasja@gmail.com>
2015-04-10 13:38:45 -0700

    Styled side menu.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,11 +5,6 @@ angular.module('starter.controllers', [])
 /* ========================== */
 .controller('AppCtrl', function($scope, $rootScope, $ionicModal, $timeout) {
 
-  /* Login code is here by default */
-
-  // Form data for the login modal
-  $scope.loginData = {};
-
   // Create the login modal that we will use later
   $ionicModal.fromTemplateUrl('templates/login.html', {
     scope: $scope
@@ -17,25 +12,14 @@ angular.module('starter.controllers', [])
     $scope.modal = modal;
   });
 
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
+  // Triggered in the sponsor modal to close it
+  $scope.closeSponsor = function() {
     $scope.modal.hide();
   };
 
-  // Open the login modal
-  $scope.login = function() {
+  // Open the sponsor modal
+  $scope.sponsorModal = function() {
     $scope.modal.show();
-  };
-
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
-
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
   };
 })
 
